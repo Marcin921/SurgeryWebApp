ea27420281180f07b64c800076dee6625289c364
Marcin <marcin23zebzda@gmail.com>
2015-11-08 22:52:27 +0100

    Utworzenie pustego projektu w Visual Studio, z bazą danych w folderze App_Data z tabelą Pacjent(format: .mdf). Dodanie prostego layout'u oraz umożliwenie rejestrowania się, logowania i wylogowywania pacjentów.

---
+++ Aplikacja/PrzychodniaMvc/PrzychodniaMvc/Controllers/PacjentController.cs
@@ -0,0 +1,67 @@
+﻿using PrzychodniaMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace PrzychodniaMvc.Controllers
+{
+    public class PacjentController : Controller
+    {
+        // GET: Pacjent
+        public ActionResult Rejestruj(Pacjent p)
+        {
+            if (!ModelState.IsValid)
+            {
+                using (PrzychodniaBDEntities dc = new PrzychodniaBDEntities())
+                {
+                    //Trzeba dopisać sprawdzenie według numeru pesel czy taki pacjent  istnieje już w bazie danych
+                    dc.Pacjent.Add(p);
+                    dc.SaveChanges();
+                    ModelState.Clear();
+                    p = null;
+                    ViewBag.Message = "Twoje konto zostało założone. Możesz się teraz zalogować.";
+                }
+            }
+            return View(p);
+        }
+
+        public ActionResult Zaloguj()
+        {
+            return View();
+        }
+        [HttpPost]
+        // GET: Pacjent
+        public ActionResult Zaloguj(Pacjent p)
+        {
+            if (p.Hasło == null || p.Login == null)
+            {
+                return View(p);
+            }
+            else
+            {
+                PrzychodniaBDEntities dc = new PrzychodniaBDEntities();
+                Pacjent pacjent = dc.Pacjent.FirstOrDefault(t => (t.Login == p.Login && t.Hasło == p.Hasło));
+                if (p != null)
+                {
+                    FormsAuthentication.SetAuthCookie(p.Login, true);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                { }
+                // If we got this far, something failed, redisplay form
+                return View(p);
+            }
+        }
+
+        public ActionResult Wyloguj()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Aplikacja/PrzychodniaMvc/PrzychodniaMvc/Controllers/PrzychodniaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PrzychodniaMvc.Controllers
+{
+    public class PrzychodniaController : Controller
+    {
+        // GET: Przychodnia
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
